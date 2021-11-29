f919c5bcd11a734022bf4b99daced00e69fe3419
iamjing66 <iamjing66@163.com>
2021-11-29 13:45:27 +0800

    fix bug

--- handlers/kbeServer/Editor/Interface/interface_user.py
+++ handlers/kbeServer/Editor/Interface/interface_user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-
 import logging
 
 import requests
@@ -10,33 +9,32 @@ from handlers.SyncServer.sockect import pro_status
 from handlers.redisServer.RedisInterface import C_ServerAddressCache
 
 
-
-def IUser_Diffusion(subcode,uid,username,data,cmode):
+def IUser_Diffusion(subcode, uid, username, data, cmode):
 
     json = {
-        "opencode":300,
-        "subcode":subcode,
-        "UID":uid,
-        "username":username,
-        "data":data
+        "opencode": 300,
+        "subcode": subcode,
+        "UID": uid,
+        "username": username,
+        "data": data
     }
     json["data"]["cmode"] = cmode
 
     Adresse = C_ServerAddressCache.GetAddress(username, cmode)
     if Adresse:
         url = "http://" + str(Adresse.decode('utf-8')) + "/postinterface"
-        print("[IUser_Diffusion] url = " , url)
-        headers = {'Connection': 'close', }
+        logging.info("[IUser_Diffusion] url = %s" % url)
+        headers = {
+            'Connection': 'close',
+        }
         res = requests.post(url=url, json=json)
-        logging.log("Diffusion opencode = "  + str(300) + ",subcode = " + str(subcode) + " username = " + username + " res = " + str(res))
+        logging.info("Diffusion opencode = " + str(300) + ",subcode = " +
+                     str(subcode) + " username = " + username + " res = " +
+                     str(res))
 
 
-
-def IUser_DiffusionDo(subcode,uid,username,data):
-    logging.log(
-        "DiffusionDo opencode = " + str(300) + ",subcode = " + str(subcode) + " username = " + username + " res = " + str(
-            data))
+def IUser_DiffusionDo(subcode, uid, username, data):
+    logging.info("DiffusionDo opencode = %s ,subcode = %s, username = %s, res = %s" % (300, subcode, username, data))
     if subcode == 1:
-        #顶号
-        pro_status.user_kick(uid,data["cmode"])
-
+        # 顶号
+        pro_status.user_kick(uid, data["cmode"])
--- handlers/redisServer/RedisInterface.py
+++ handlers/redisServer/RedisInterface.py
@@ -8,6 +8,7 @@ import time
 
 import Global
 
+
 class RedisBase(object):
     """缓存类父类"""
     _host = Global.RedisCreatex_options["host"]
@@ -18,52 +19,51 @@ class RedisBase(object):
     @property
     def redis_ctl(self):
         """redis控制句柄,就是连接对象"""
-        redis_ctl = redis.Redis(host=self._host, port=self._port, db=self._database, password=self._password)
+        redis_ctl = redis.Redis(host=self._host,
+                                port=self._port,
+                                db=self._database,
+                                password=self._password)
         return redis_ctl
-
 
 
 #检测写入公共配置
 class RedisWorker(RedisBase):
-
     def GetWorkMainState(self):
 
         return self.redis_ctl.get("mainserver")
 
-
-
     def SetWorkMain(self):
-        self.redis_ctl.set("mainserver",str(int(time.time())))
+        self.redis_ctl.set("mainserver", str(int(time.time())))
 
+        # ServerRt - 0-本地测试 1-外网正式 2-外网测试
+        # def WriteConfig(self):
 
-    #ServerRt - 0-本地测试 1-外网正式 2-外网测试
-    #def WriteConfig(self):
+        #     配置
+        #     name = cxconfig
 
-        #配置
-        #name = cxconfig
+        #     写入 负载地址
+        #     name = balancing
+        #     self.redis_ctl.decr("balancing")
+        #     self.redis_ctl.set("balancing",Global.GetConfig(ServerRt,0))
+        #     self.redis_ctl.hdel("cxconfig","balancing")
+        #     print(Global.GetConfig(0))
+        self.redis_ctl.hset("cxconfig", "balancing",
+                            Global.get_config.redis_config())
 
-        #写入 负载地址
-        #name = balancing
-        #self.redis_ctl.decr("balancing")
-        #self.redis_ctl.set("balancing",Global.GetConfig(ServerRt,0))
-        #self.redis_ctl.hdel("cxconfig","balancing")
-        #print(Global.GetConfig(0))
-        self.redis_ctl.hset("cxconfig","balancing",Global.get_config.redis_config)
-        #l = self.redis_ctl.hget("cxconfig","balancing")
-        #print("list = " , list(l))
-
+    #     l = self.redis_ctl.hget("cxconfig","balancing")
+    #     print("list = " , list(l))
 
 
 class ServerAddressCache(RedisBase):
+    def SetUser(self, username, cmode, Adresse):
+        self.redis_ctl.hset(username, cmode, Adresse)
 
+    def GetAddress(self, username, cmode):
+        return self.redis_ctl.hget(username, cmode)
 
-    def SetUser(self,username,cmode,Adresse):
-        self.redis_ctl.hset(username,cmode,Adresse)
-
-    def GetAddress(self,username,cmode):
-        return self.redis_ctl.hget(username,cmode)
 
 C_ServerAddressCache = ServerAddressCache()
+
 
 class TokenCache(RedisBase):
     """微信token缓存"""
@@ -102,7 +102,7 @@ class RedisData():
     def redis_pool(self):
         print(self.redis_config)
         rdp = redis.ConnectionPool(self.redis_config)
-        #rdc = redis.Redis(self.redis_config)
-        #print(rdc)
-        rdc = redis.StrictRedis(connection_pool=rdp, encoding='utf8', decode_responses=True)
+        rdc = redis.StrictRedis(connection_pool=rdp,
+                                encoding='utf8',
+                                decode_responses=True)
         return rdc
